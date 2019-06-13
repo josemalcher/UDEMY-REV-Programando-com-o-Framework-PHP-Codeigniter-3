66fc0312138fa1cf9c7d2b61d1e71b6732cf9032
José Malcher Jr <contato@josemalcher.net>
2019-06-13 18:35:44 -0300

    2. Instalando o banco de dados e modelando o primeiro Model

--- myapp/application/controllers/Home.php
+++ myapp/application/controllers/Home.php
@@ -6,11 +6,15 @@ class Home extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
+
+        $this->load->model('categorias_model', 'modelcategorias');
+        $this->categorias = $this->modelcategorias->listar_categorias();
     }
 
     public function index()
 	{
-	    $this->load->view('frontend/template/html-header');
+	    $dados['categorias'] = $this->categorias;
+	    $this->load->view('frontend/template/html-header', $dados);
 	    $this->load->view('frontend/template/header');
 	    $this->load->view('frontend/home');
 	    $this->load->view('frontend/template/aside');
